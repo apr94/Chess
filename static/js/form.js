e5cba16ab57c88ef270bc8ccb74dd5e0cb29a0d8
Palimar Rao <palimar@seas.upenn.edu>
2013-12-07 21:44:50 -0500

    Added Game Form

--- static/js/form.js
+++ static/js/form.js
@@ -1,5 +1,51 @@
- $(document).ready(function() {
- 	$('.NewGameButton').click(function() {
- 		document.getElementById("NewGameForm").submit();
- 	});
- });
+
+$(document).ready(function() {
+	$('.StartGameButton').click(function() {
+		document.getElementById("StartGameForm").submit();
+	});
+});
+
+$(document).ready(function() {
+	$('.NewGameButton').click(function() {
+		document.getElementById("NewGameForm").submit();
+	});
+});
+
+
+
+
+
+
+$(document).ready(function ()
+{
+
+	for(var i = 0; i < 61; i++){
+		$('#MinutesList').append("<option value = \"" + i + "\">" + i + "</option>");
+		$('#SecondsList').append("<option value = \"" + i + "\">" + i + "</option>");
+		$('#IncrementList').append("<option value = \"" + i + "\">" + i + "</option>");	
+	}
+
+
+
+	$('#TimeControls').hide();
+})
+
+
+$(document).ready(function ()
+{
+	$("input[name='Timing']").change(radioValueChanged);
+})
+
+function radioValueChanged()
+{
+	radioValue = $(this).val();
+
+	if($(this).is(":checked") && radioValue == "UnTimed")
+	{
+		$('#TimeControls').hide();
+	}
+	else
+	{
+		$('#TimeControls').show();
+	}
+} 
